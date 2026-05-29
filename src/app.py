7483941b80de192e4f93827d81b8f90481057180
Kyle <kdanahy33@gmail.com>
2026-05-29 12:52:13 -0600

    Adding pydoc

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""A simple Flask app that echoes user input back to the user."""
 
 from flask import Flask, request
 
@@ -6,6 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
+    """Main page with a form to submit user input."""
     return '''
      <form action="/echo_user_input" method="POST">
          <input name="user_input">
@@ -15,5 +17,6 @@ def main():
 
 @app.route("/echo_user_input", methods=["POST"])
 def echo_input():
+    """Echoes the user input back to the user."""
     input_text = request.form.get("user_input", "")
     return "You entered: " + input_text
